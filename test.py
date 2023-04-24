965d51b2b1f38cb6551516d329b8e286402719d1
GAI-313 <nakatogawagai@gmail.com>
2023-04-24 11:30:23 +0900

    solved pythonpath

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-from modules.tello import console
+from tello import console
 
-drone = console(language="jp", recv=False)
-
-while True:
-    print(drone.get_imu(),
-            drone.get_tof(),
-            drone.get_height(),
-            drone.get_battery(),
-            drone.get_flighttime(),
-            drone.get_speed())
+drone = console()
